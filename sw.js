7ba74aeafa24de4babdc4a6578a548ecd8dbd198
Meiling Tang <meilingxt@gmail.com>
2019-08-08 21:16:44 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,37 +26,37 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-48db9b2f64d627346113.js"
+    "url": "webpack-runtime-c0626b876f1d986c30d8.js"
   },
   {
     "url": "styles.2f91a798493a13edcd2c.css"
   },
   {
-    "url": "styles-961aeb459a34f74bea36.js"
+    "url": "styles-e505cf47bda8ee0c73d0.js"
   },
   {
-    "url": "app-1e9b0dc5da12cb0655f1.js"
+    "url": "app-fa4ae968144f7fa03cf3.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-1595fd9bcd9902e26bdf.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2b2f2ade9c04c32a38b8acae633c29e5"
+    "revision": "74d87081347985ecd2381631c7d9e206"
   },
   {
     "url": "1-74f3327beb65c7b3a490.js"
   },
   {
-    "url": "component---src-templates-not-found-template-js-c9a7275768995db2205c.js"
+    "url": "component---src-templates-not-found-template-js-04a6ee34154d0d916a75.js"
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "6f57473a706976129205a3589976a26e"
+    "revision": "3b47f3bbdfbaf1ffcae16af21ee55ae2"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "895fea9d67ca1155eded06ddfb27ed33"
+    "revision": "d55d9c473ce59e309ea127b968c58f4d"
   },
   {
     "url": "manifest.json",
@@ -64,7 +64,7 @@ self.__precacheManifest = [
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "c4877bcdb772d17f45d97ad9f8091fa5"
+    "revision": "8c0dc75f801dbb3302bda07c49c72366"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
